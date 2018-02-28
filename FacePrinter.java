795d67df6909ee915524bb9fc5a4a2890909716f
BottledDew <36426880+BottledDew@users.noreply.github.com>
2018-02-28 00:49:39 -0800

    Add files via upload

---
+++ FacePrinter.java
@@ -0,0 +1,17 @@
+//Programmer: Nguyen Li
+//Assignment: FacePrinter
+//Due Date: February 5, 2018
+
+public class FacePrinter
+{
+	public static void main(String[] args)
+	{
+	System.out.println(" /| /| /| /|");
+	System.out.print("| --\t -- |\n");
+	System.out.print("| O\t O  |\n");
+	System.out.print("|(   ^\t   )|\n");
+	System.out.print("|   [__]    |\n");
+	System.out.print("|\\\t   /|\n");
+	System.out.println("  ---------");
+	}
+}
